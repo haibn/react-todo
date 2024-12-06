4ff388f9396b5af9aa831fd01c9e0fd5ba0bd577
haibn <haibinyuy@berkeley.edu>
2024-12-06 12:53:18 -0800

    saved todo-list in local storage

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,19 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import TodoList from './TodoList'
 import AddTodoForm from './AddTodoForm'
 
+function useSemiPersistentState(key, value) {
+  let [todoList, setTodoList] = useState(JSON.parse(localStorage.getItem("savedTodoList")) || []);
+  useEffect(() => localStorage.setItem("savedTodoList", JSON.stringify(todoList)), [todoList]);
+
+  return [todoList, setTodoList]
+}
+
 function App() {
-  let [todoList, setTodoList] = useState([]);
+  let [todoList, setTodoList] = useSemiPersistentState();
 
   function addTodo(newTodo) {
     setTodoList([newTodo, ...todoList])
