9b73529fb90b83e4f0a29ae4e9c2bb4be44066e0
haibn <haibinyuy@berkeley.edu>
2025-01-16 14:14:29 -0800

    fetching data from airtable

--- src/App.jsx
+++ src/App.jsx
@@ -11,19 +11,42 @@ function App() {
   // Loading state
   const [isLoading, setIsLoading] = useState(true);
 
+  // Fetching data from AirTable
+  async function fetchData() {
+    let options = {
+      method : "GET",
+      headers: {
+        Authorization: `Bearer ${import.meta.env.VITE_AIRTABLE_API_TOKEN}`
+      }
+    }
+
+    const url = `https://api.airtable.com/v0/${import.meta.env.VITE_AIRTABLE_BASE_ID}/${import.meta.env.VITE_TABLE_NAME}`;
+
+    try {
+      const response = await fetch(url, options);
+
+      if (!response.ok) {
+        throw new Error(`Error ${response.status}`);
+      }
+      
+      const data = await response.json();
+      const todos = data.records.map((todo) => ({
+        title : todo.fields.Title,
+        id: todo.id
+      }));
+      console.log(todos);
+
+      setTodoList(todos);
+      setIsLoading(false);
+      
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
   // Load initial todoList state from saved local storage.
   useEffect(() => {
-    const fetchData = new Promise((resolve, reject) => {
-      setTimeout(() => resolve({
-        data: {
-          todoList: JSON.parse(localStorage.getItem("savedTodoList")) || []
-        }
-      }), 2000);
-    })
-    fetchData.then((response) => {
-      setTodoList(response.data.todoList);
-      setIsLoading(false);
-    });
+    fetchData();
   }, []);
 
   // Update local storage with most recent todoList
