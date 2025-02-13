1bc1ce2731e2ef602498deafe2c45550c270176f
haibn <haibinyuy@berkeley.edu>
2025-02-12 16:16:54 -0800

    added prop types

--- src/components/AddTodoForm.jsx
+++ src/components/AddTodoForm.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import InputWithLabel from "./InputWithLabel";
 import styles from "./AddTodoForm.module.css"
+import PropTypes from "prop-types";
 
 function AddTodoForm({onAddTodo}) {
     let [todoTitle, setTodoTitle] = useState("");
@@ -26,6 +27,10 @@ function AddTodoForm({onAddTodo}) {
             </form>
         </div>
     )
+}
+
+AddTodoForm.propTypes = {
+    onAddTodo : PropTypes.func
 }
 
 export default AddTodoForm
